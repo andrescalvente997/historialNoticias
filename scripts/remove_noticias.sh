51a9799983724fd82591f1dc606da136921f23b8
Andrés Calvente <andres.calvente@estudiante.uam.es>
2020-01-24 12:30:10 +0100

    Creado script de borrado de noticias
    Este script que he creado sirve para eliminar todos los archivos
    de noticias que haya en los distintos periódicos.

--- scripts/remove_noticias.sh
+++ scripts/remove_noticias.sh
@@ -7,15 +7,23 @@ ruta_20_MINUTOS="$DIR/../crawler/crawlerPeriodicos/datos_20_MINUTOS"
 ruta_EL_CONFIDENCIAL="$DIR/../crawler/crawlerPeriodicos/datos_EL_CONFIDENCIAL"
 ruta_MARCA="$DIR/../crawler/crawlerPeriodicos/datos_MARCA"
 
-echo "¿Estás seguro de borrar todas las noticias?(S/N)"
+echo "¿Estás seguro de borrar todas las noticias?(S/n)"
 
 read confirmacion
 confirmacion=$(echo "$confirmacion" | tr '[:upper:]' '[:lower:]')
 
-if [$confirmacion="s"]
+if [ "$confirmacion" = "s" ] || [ "$confirmacion" = "si" ] || [ "$confirmacion" = "y" ] || [ "$confirmacion" = "yes" ];
 then
-    echo "SI"
+    rm -r "$ruta_EL_PAIS/"
+    mkdir "$ruta_EL_PAIS/"
+    rm -r "$ruta_EL_MUNDO/"
+    mkdir "$ruta_EL_MUNDO/"
+    rm -r "$ruta_20_MINUTOS/"
+    mkdir "$ruta_20_MINUTOS/"
+    rm -r "$ruta_EL_CONFIDENCIAL/"
+    mkdir "$ruta_EL_CONFIDENCIAL/"
+    rm -r "$ruta_MARCA/"
+    mkdir "$ruta_MARCA/"
 else
-    echo "NO"
+    echo "Abortando operación..."
 fi
-exit
