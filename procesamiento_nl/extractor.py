5b0bcf2be7351e3f821a56634f3fa4d4b6dcbc62
Andrés Calvente <andres.calvente@estudiante.uam.es>
2020-02-04 13:41:10 +0100

    Añadida funcionalidad de busqueda de noticia a operar y busqueda de noticias

--- procesamiento_nl/extractor.py
+++ procesamiento_nl/extractor.py
@@ -14,28 +14,59 @@ REGEX_PUNCTUATION = re.compile('[%s]' % re.escape(string.punctuation))
 
 class Extractor():
 
-    def __init__(self, fileStr):
+    def __init__(self, fileStr, linkNoticia):
 
         self.nlp = spacy.load("es_core_news_md")
-    
-        self.fileStr = fileStr
-        self.jsonFile, self.data = self.openFile()
+
+        self.jsonFile, self.dataNoticias = self.openFile()
 
         aux_regex = r'\b'
         aux_regex += r'\b|\b'.join(STOP_WORDS)
         aux_regex += r'\b'
         self.regex_stopwords = re.compile('%s' % aux_regex)
 
+        self.index = 0
 
-    def openFile(self):
+        self.dataNoticiaMaster = self.searchNoticiaMaster(linkNoticia)
 
-        file = open(self.fileStr, 'r')
+
+    def getNextNoticia(self):
+
+        dataNextNoticia = self.dataNoticias[self.index]
+
+        if dataNextNoticia['linkNoticia'] == self.linkNoticia:
+            self.index += 1
+            dataNextNoticia = self.dataNoticias[self.index]
+        
+        flgEnd = False
+        while flgEnd != True:
+            # Comparamos la fecha de las noticias
+            if dataNextNoticia['fechaPublicacionNoticia'] > self.dataNoticiaMaster['fechaPublicacionNoticia'] 
+                self.index += 1
+                dataNextNoticia = self.dataNoticias[self.index]
+            else:
+                flgEnd = True
+
+        self.index += 1
+
+        return dataNextNoticia   
+
+    def openFile(self, fileStr):
+
+        file = open(fileStr, 'r')
         return file, json.loads(file)
 
 
     def closeFile(self):
 
         self.jsonFile.close()
+
+    
+    def searchNoticiaMaster(self, linkNoticia):
+
+        for noticia in self.dataNoticias:
+            if noticia['linkNoticia'] == linkNoticia:
+                return noticia
 
     
     def rm_stopWords(self, doc):
