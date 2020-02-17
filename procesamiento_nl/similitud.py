8ace60554f0a94664c9aaf0f7d393117c5b866ae
Andrés Calvente <andres.calvente@estudiante.uam.es>
2020-02-17 17:59:51 +0100

    Añadido la funcion de calcular la similitud Jaccard entre textos

--- procesamiento_nl/similitud.py
+++ procesamiento_nl/similitud.py
@@ -11,7 +11,18 @@ class Similitud():
     def __init__(self):
         pass
 
-
+    
     def similitud_spacy(self, doc1, doc2, redondeo=5):
 
         return round(doc1.similarity(doc2), redondeo)
+
+
+    def similitud_jaccard(self, doc1, doc2, redondeo=5):
+        
+        s1 = set(map(lambda token: token.text, doc1))
+        s2 = set(map(lambda token: token.text, doc2))
+
+        elemsInterseccion = len(s1 & s2)
+        elemsUnion = len(s1 | s2)
+
+        return elemsInterseccion / elemsUnion
