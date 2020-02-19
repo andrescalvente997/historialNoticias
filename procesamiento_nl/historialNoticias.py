4564c31ca2393c650ea9790a8035d7f218311677
Andrés Calvente <andres.calvente@estudiante.uam.es>
2020-02-19 22:51:36 +0100

    Solucionado errores de ejecucion

--- procesamiento_nl/historialNoticias.py
+++ procesamiento_nl/historialNoticias.py
@@ -9,34 +9,41 @@ import procesador
 
 ATRIBUTO_ESTUDIO_1 = "cuerpoNoticia"
 TOP_RESULTS = 100
+URL_NOTICIA_ANALIZAR = "https://elpais.com/sociedad/2020/02/01/actualidad/1580569994_549942.html"
+NOTICIA_FILEPATH = dirname(abspath(__file__)) + "/" + "../crawler/crawlerPeriodicos/datos_EL_PAIS/20200125_20200202_noticias.json"
 
-def do_similitud(extractor, funct_similitud, nombreSimilitud):
+def do_similitud(funct_similitud, nombreSimilitud):
+
+    extractor_obj = extractor.Extractor(NOTICIA_FILEPATH, URL_NOTICIA_ANALIZAR)
 
     procesador_obj = procesador.Procesador(nombreSimilitud)
 
     time_start = time.time()
 
-    dataNoticia_Master = extractor.getNoticiaMaster()
-    atributoNoticia_Master = extractor.getAtributoNoticia(dataNoticia_Master, ATRIBUTO_ESTUDIO_1)
-    dataNoticia_Analizar = extractor.getNextNoticia()
+    dataNoticia_Master = extractor_obj.getNoticiaMaster()
+    atributoNoticia_Master = extractor_obj.getAtributoNoticia(dataNoticia_Master, ATRIBUTO_ESTUDIO_1)
+    dataNoticia_Analizar = extractor_obj.getNextNoticia()
+
     while dataNoticia_Analizar != -1:
         
-        linkNoticia = extractor.getAtributoNoticia(dataNoticia_Analizar, "linkNoticia", flgTratar=False)
-        atributoNoticia_Analizar = extractor.getAtributoNoticia(dataNoticia_Analizar, ATRIBUTO_ESTUDIO_1)
+        linkNoticia = extractor_obj.getAtributoNoticia(dataNoticia_Analizar, "linkNoticia", flgTratar=False)
+        atributoNoticia_Analizar = extractor_obj.getAtributoNoticia(dataNoticia_Analizar, ATRIBUTO_ESTUDIO_1)
 
         if atributoNoticia_Analizar == None:
             #print(">> Noticia sin Atributo - " + strAtributo + ": " + linkNoticia)
-            dataNoticia_Analizar = extractor.getNextNoticia()
+            dataNoticia_Analizar = extractor_obj.getNextNoticia()
             continue
 
         score = funct_similitud(atributoNoticia_Master, atributoNoticia_Analizar)
         procesador_obj.addResultado(linkNoticia, score)
 
-        dataNoticia_Analizar = extractor.getNextNoticia()
+        dataNoticia_Analizar = extractor_obj.getNextNoticia()
 
     time_end = time.time()
     
     printResult(procesador_obj, round(time_end - time_start))
+
+    return
 
 
 def printResult(procesador_obj, executionTime):
@@ -54,15 +61,13 @@ def printResult(procesador_obj, executionTime):
     print(">> Tiempo de ejecución:")
     print("{} minutos y {} segundos.\n".format(str(mins), str(segs)))
 
+    return
+
 
 if __name__ == '__main__':
 
-    strFile = dirname(abspath(__file__)) + "/" + "../crawler/crawlerPeriodicos/datos_EL_PAIS/20200125_20200202_noticias.json"
-    extractor = extractor.Extractor(strFile, "https://elpais.com/sociedad/2020/02/01/actualidad/1580569994_549942.html")
-    
     similitud_obj = similitud.Similitud()
-    do_similitud(extractor, similitud_obj.similitud_spacy, "Similitud_Coseno_Spacy")
-    
-    similitud_obj = similitud.Similitud()
-    do_similitud(extractor, similitud_obj.similitud_jaccard, "Similitud_Jaccard")
+
+    do_similitud(similitud_obj.similitud_spacy, "Similitud_Coseno_Spacy")
+    do_similitud(similitud_obj.similitud_jaccard, "Similitud_Jaccard")
 
