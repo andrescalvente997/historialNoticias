94d0a673d04b79998cf3983ffebf189536c21819
Andrés Calvente <andres.calvente@estudiante.uam.es>
2020-02-20 19:29:11 +0100

    Añadidas las funcionalidades necesarias para poder probar la implementación de tf-idf

--- procesamiento_nl/historialNoticias.py
+++ procesamiento_nl/historialNoticias.py
@@ -15,9 +15,7 @@ NOTICIA_FILEPATH = dirname(abspath(__file__)) + "/" + "../crawler/crawlerPeriodi
 def do_similitud(similitud_obj):
 
     extractor_obj = extractor.Extractor(NOTICIA_FILEPATH, URL_NOTICIA_ANALIZAR)
-
     procesador_obj = procesador.Procesador(similitud_obj.getNombreSimilitud())
-
     funct_similitud = similitud_obj.getFuncionSimilitud()
 
     time_start = time.time()
@@ -48,6 +46,49 @@ def do_similitud(similitud_obj):
     return
 
 
+def do_similitud_tfidf(similitud_obj):
+
+    extractor_obj = extractor.Extractor(NOTICIA_FILEPATH, URL_NOTICIA_ANALIZAR)
+    procesador_obj = procesador.Procesador(similitud_obj.getNombreSimilitud())
+    funct_similitud = similitud_obj.getFuncionSimilitud()
+
+    time_start = time.time()
+
+    dataNoticia_Master = extractor_obj.getNoticiaMaster()
+    atributoNoticia_Master = extractor_obj.getAtributoNoticia(dataNoticia_Master, ATRIBUTO_ESTUDIO_1)
+    linkNoticia_Master = extractor_obj.getAtributoNoticia(dataNoticia_Master, "linkNoticia", flgTratar=False)
+    similitud_obj.add_doc_wFrec_entry(linkNoticia_Master, atributoNoticia_Master)
+
+    list_linksNoticiasAnalizar = []
+    dataNoticia_Analizar = extractor_obj.getNextNoticia()
+
+    while dataNoticia_Analizar != -1:
+        
+        linkNoticia_Analizar = extractor_obj.getAtributoNoticia(dataNoticia_Analizar, "linkNoticia", flgTratar=False)
+        list_linksNoticiasAnalizar.append(linkNoticia_Analizar)
+        atributoNoticia_Analizar = extractor_obj.getAtributoNoticia(dataNoticia_Analizar, ATRIBUTO_ESTUDIO_1)
+
+        if atributoNoticia_Analizar == None:
+            #print(">> Noticia sin Atributo - " + strAtributo + ": " + linkNoticia)
+            dataNoticia_Analizar = extractor_obj.getNextNoticia()
+            continue
+
+        similitud_obj.add_doc_wFrec_entry(linkNoticia_Analizar, dataNoticia_Analizar)
+
+    similitud_obj.create_dicc_doc_tfidf()
+
+    for linkNoticia_Analizar in list_linksNoticiasAnalizar:
+
+        score = funct_similitud(linkNoticia_Master, linkNoticia_Analizar)
+        procesador_obj.addResultado(linkNoticia_Analizar, score)
+
+    time_end = time.time()
+    
+    printResult(procesador_obj, round(time_end - time_start))
+
+    return
+
+
 def printResult(procesador_obj, executionTime):
 
     procesador_obj.sortResultados()
@@ -57,6 +98,7 @@ def printResult(procesador_obj, executionTime):
 
     print("#########################################################\n")
     print(">> Resumen:")
+    print("Estudiando el atributo '{}'".format(ATRIBUTO_ESTUDIO_1))
     print(procesador_obj)
     print(">> Top:")
     print(strResultTop)
@@ -73,4 +115,7 @@ if __name__ == '__main__':
 
     similitud_obj = similitud.Similitud("SIMILITUD_JACCARD")
     do_similitud(similitud_obj)
+
+    similitud_obj = similitud.Similitud("SIMILITUD_COSENO_TF-IDF")
+    do_similitud_tfidf(similitud_obj)
 
