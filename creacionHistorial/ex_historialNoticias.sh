42699dbdd281ce9440fb6798c962286d30eb80f5
Andrés Calvente <andres.calvente@estudiante.uam.es>
2020-06-13 20:14:09 +0200

    Preparada la ejecución de todas las similitudes

--- creacionHistorial/ex_historialNoticias.sh
+++ creacionHistorial/ex_historialNoticias.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ruta_PROCESAMIENTO_LN="$DIR/../creacionHistorial"
-
-cd "$ruta_PROCESAMIENTO_LN"
-
-workon TFG
-
 python historialNoticias.py EL_CONFIDENCIAL https://www.elconfidencial.com/espana/cataluna/2019-10-20/quim-torra-vuelve-a-llamar-a-pedro-sanchez-sin-obtener-respuesta_2291844/ JUICIO_PROCÉS 01
 python historialNoticias.py EL_MUNDO https://www.elmundo.es/internacional/2020/02/09/5e405358fdddff0f278b45bd.html BREXIT 02
 python historialNoticias.py EL_PAIS https://elpais.com/cultura/2019/04/27/actualidad/1556380153_549141.html INCENDIO_NÔTRE_DAME 03
